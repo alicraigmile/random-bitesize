71c0ebb2d9d3e0ef20a01e8ff7c3d7f6a060af94
Ali Craigmile <ali@craigmile.com>
2017-06-15 00:18:31 +0100

    github ribbon

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ var _sendHtml = function(req, res, items) {
     items.forEach(function(item) {
         html += '<li><a href="' + item.url + '">' + item.title + '</a></li>';
     });
-    html += '</ul></body></html>';
+    html += '</ul><a href="https://github.com/alicraigmile/random-bitesize"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a></body></html>';
 
     res.set('Content-Type', 'text/html');
     res.send(html);

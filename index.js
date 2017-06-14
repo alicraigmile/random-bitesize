20de34aa8082a22208e456698f3aa200eea5c799
Ali Craigmile <ali@craigmile.com>
2017-06-14 23:20:57 +0100

    added /daily/rss which updates once every 24hrs

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ Array.prototype.randomItem = function () {
 const fs = require('fs'),
     pkg = require('./package'),
     express = require('express'),
+    NodeCache = require( "node-cache" ),
+    myCache = new NodeCache();
     app = express(),
+    ttl = 60*60*24,
     port = process.env.PORT || 3000;
 
 app.get('/', randomTopicHtml);
 app.get('/json', randomTopicJson);
 app.get('/rss', randomTopicRss);
+app.get('/daily/rss', randomTopicDailyRss);
 app.get('/go', randomTopicRedirect);
 app.get('/status', status);
 console.log('Started on http://localhost:' + port + ' (Ctrl-C to quit)');
@@ -35,15 +39,49 @@ function randomTopicHtml (req, res) {
     res.send('<body><h1>' + name + '</h1><p>' + description + '</p><h2>Random topic</h2><ul><li><a href="' + url + '">' + url + '</a></li></ul></body>');
 }
 
+var _sendRss = function(req, res, items) {
+    var rss = '';
+
+    rss = '<?xml version="1.0" encoding="utf-8"?><rss version="2.0"><channel><title>' + pkg.name + '</title><description>' + pkg.description + '</description><link>http://random-bitesize.herokuapp.com/</link><ttl>' + ttl + '</ttl>';
+    items.forEach(function(item) {
+        rss += '<item><title>' + item.title + '</title><link>' + item.url + '</link></item>';
+    });
+    rss += '</channel></rss>';
+
+    res.set('Content-Type', 'application/rss+xml');
+    res.send(rss);
+}
+
 function randomTopicRss (req, res) {
-		var url = _randomTopic(),
-        name = pkg.name,
-        description = pkg.description,
-				ttl = 1800;
+    var url = _randomTopic();
 
-		res.set('Content-Type', 'application/rss+xml');
+    _sendRss(req, res, url);
+}
 
-    res.send('<?xml version="1.0" encoding="utf-8"?><rss version="2.0"><channel><title>' + name + '</title><description>' + description + '</description><link>http://random-bitesize.herokuapp.com/</link><ttl>' + ttl + '</ttl><item><title>' + url + '</title><link>' + url + '</link></item></channel></rss>');
+function randomTopicDailyRss (req, res) {
+       const cacheKey = '24hrs',
+             cacheUpdate = function() {
+                var url = _randomTopic(),
+                    title = "Today's random bitesize page";
+                return [{url, title}];
+             };
+
+        myCache.get(cacheKey, function(err, value) {
+            if (!err)
+                if(value == undefined) {
+                    value = cacheUpdate();
+                    myCache.set(cacheKey, value, ttl, function(err, success) {
+                        if (!err && success) {
+                            _sendRss(req,res,value);
+                        } else {
+                            req.send('error');
+                        }
+                    });
+                } else {
+                    _sendRss(req,res,value);
+                }
+                    
+    });
 }
 
 function randomTopicJson (req, res) {

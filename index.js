038ef0784b23544ae14a9c9e2e53fbc49f8cd7ae
Ali Craigmile <ali.craigmile@bbc.co.uk>
2025-08-07 10:33:15 +0100

    refactor + modernise js

--- index.js
+++ index.js
@@ -6,22 +6,17 @@ const fs = require('fs'),
     myCache = new NodeCache();
     app = express(),
     ttl = 60*60*24,
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    rndm = (x) => x[Math.floor(Math.random() * x.length)],
+    strings = {
+        'forkMeOnGithub': 'Fork me on GitHub',
+        'aRandomBitesizeTopic': 'A random bitesize topic',
+        'todaysRandomBitesizeTopic': "Today's random bitesize topic",
+        'githubUrl': "https://github.com/alicraigmile/random-bitesize"
+    };
 
-let rndm = (x) => x[Math.floor(Math.random() * x.length)];
-
-app.get('/', randomTopicHtml);
-app.get('/json', randomTopicJson);
-app.get('/rss', randomTopicRss);
-app.get('/daily', randomTopicDaily);
-app.get('/daily/go', randomTopicDailyRedirect);
-app.get('/daily/rss', randomTopicDailyRss);
-app.get('/go', randomTopicRedirect);
-app.get('/status', status);
-
-// index.js
-function randomTopic () {
-    var folder = 'data/curriculum-data/',
+const randomTopic = () => {
+    const folder = 'data/curriculum-data/',
         allTurtles = fs.readdirSync(folder),
         randomTurtle = rndm(allTurtles),
         fileContents = fs.readFileSync(folder + randomTurtle, {encoding: 'utf8'}),
@@ -31,119 +26,120 @@ function randomTopic () {
     return randomFind;
 }
 
-function randomTopicHtml (req, res) {
-        var url = randomTopic(),
-            title = "A random bitesize page",
+const randomTopicHtml = (req, res) => {
+        const url = randomTopic(),
+            title = strings.aRandomBitesizeTopic,
             listItems =  [{url: url, title: title}]; //there can only be one
 
-        _sendHtml(req,res,listItems);
+        sendHtml(req,res,listItems);
 }
 
-var _sendRss = function(req, res, items) {
-    var rss = '';
+const sendRss = (req, res, items) => {
+    const appurl = 'http://random-bitesize.herokuapp.com/';
+    let rss = '';
 
-    rss = '<?xml version="1.0" encoding="utf-8"?><rss version="2.0"><channel><title>' + pkg.name + '</title><description>' + pkg.description + '</description><link>http://random-bitesize.herokuapp.com/</link><ttl>' + ttl + '</ttl>';
-    items.forEach(function(item) {
-        rss += '<item><title>' + item.title + '</title><link>' + item.url + '</link></item>';
+    rss = `<?xml version="1.0" encoding="utf-8"?><rss version="2.0"><channel><title>${pkg.name}</title><description>${pkg.description}</description><link>${appurl}</link><ttl>${ttl}</ttl>`;
+    items.forEach((item) => {
+        rss += `<item><title>${item.title}</title><link>${item.url}</link></item>`;
     });
-    rss += '</channel></rss>';
+    rss += `</channel></rss>`;
 
     res.set('Content-Type', 'application/rss+xml');
     res.send(rss);
 }
 
-var _sendHtml = function(req, res, items) {
-    var html = '',
-        name = pkg.name,
+const sendHtml = (req, res, items) => {
+    const name = pkg.name,
         description = pkg.description;
+    
+    let html = '';
 
-    html = '<!DOCTYPE html><html><body><h1>' + name + '</h1><p>' + description + '</p><ul>';
-    items.forEach(function(item) {
-        html += '<li><a href="' + item.url + '">' + item.title + '</a></li>';
+    html = `<!DOCTYPE html><html><body><h1>${name}</h1><p>${description}</p><ul>`;
+    items.forEach((item) => {
+        html += `<li><a href="${item.url}">${item.title}</a></li>`;
     });
-    html += '</ul><a href="https://github.com/alicraigmile/random-bitesize"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a></body></html>';
+    html += `</ul><a href="${strings.githubUrl}"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="${strings.forkMeOnGithub}" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a></body></html>`;
 
     res.set('Content-Type', 'text/html');
     res.send(html);
 }
  
 
-function randomTopicRss (req, res) {
-    var url = randomTopic(),
-        title = "A random bitesize page",
+const randomTopicRss = (req, res) => {
+    const url = randomTopic(),
+        title = strings.aRandomBitesizeTopic,
         rssItems =  [{url: url, title: title}]; //there can only be one
 
-    _sendRss(req, res, rssItems);
+    sendRss(req, res, rssItems);
 }
 
-function randomTopicDaily (req, res) {
+const randomTopicDaily = (req, res) => {
        const cacheKey = '24hrs',
-             cacheUpdate = function() {
-                var url = randomTopic(),
-                    title = "Today's random bitesize page";
+             cacheUpdate = () => {
+                const url = randomTopic(),
+                    title = strings.todaysRandomBitesizeTopic;
                 return [{url:url, title:title}];
              };
 
-        myCache.get(cacheKey, function(err, value) {
+        myCache.get(cacheKey, (err, value) => {
             if (!err)
                 if(value == undefined) {
                     value = cacheUpdate();
-                    myCache.set(cacheKey, value, ttl, function(err, success) {
+                    myCache.set(cacheKey, value, ttl, (err, success) => {
                         if (!err && success) {
-                            _sendHtml(req,res,value);
+                            sendHtml(req,res,value);
                         } else {
                             req.send('error');
                         }
                     });
                 } else {
-                    _sendHtml(req,res,value);
+                    sendHtml(req,res,value);
                 }
                     
     });
 }
 
-
-function randomTopicDailyRss (req, res) {
+const randomTopicDailyRss = (req, res) => {
        const cacheKey = '24hrs',
-             cacheUpdate = function() {
-                var url = randomTopic(),
-                    title = "Today's random bitesize page";
+             cacheUpdate = () => {
+                const url = randomTopic(),
+                    title = strings.todaysRandomBitesizeTopic;
                 return [{url: url, title: title}];
              };
 
-        myCache.get(cacheKey, function(err, value) {
+        myCache.get(cacheKey, (err, value) => {
             if (!err)
                 if(value == undefined) {
                     value = cacheUpdate();
-                    myCache.set(cacheKey, value, ttl, function(err, success) {
+                    myCache.set(cacheKey, value, ttl, (err, success) => {
                         if (!err && success) {
-                            _sendRss(req,res,value);
+                            sendRss(req,res,value);
                         } else {
                             req.send('error');
                         }
                     });
                 } else {
-                    _sendRss(req,res,value);
+                    sendRss(req,res,value);
                 }
 
     });
 }
 
-function randomTopicDailyRedirect (req, res) {
+const randomTopicDailyRedirect = (req, res) => {
        const cacheKey = '24hrs',
-             cacheUpdate = function() {
-                var url = randomTopic(),
-                    title = "Today's random bitesize page";
+             cacheUpdate = () => {
+                const url = randomTopic(),
+                    title = strings.todaysRandomBitesizeTopic;
                 return [{url: url, title: title}];
              };
 
-        myCache.get(cacheKey, function(err, value) {
+        myCache.get(cacheKey, (err, value) => {
             if (!err)
                 if(value == undefined) {
                     value = cacheUpdate();
-                    myCache.set(cacheKey, value, ttl, function(err, success) {
+                    myCache.set(cacheKey, value, ttl, (err, success) => {
                         if (!err && success) {
-                            _sendRss(req,res,value);
+                            sendRss(req,res,value);
                             res.redirect(value[0].url);
                         } else {
                             req.send('error');
@@ -156,22 +152,32 @@ function randomTopicDailyRedirect (req, res) {
     });
 }
 
-function randomTopicJson (req, res) {
-	var url = randomTopic();
+const randomTopicJson = (req, res) => {
+	const url = randomTopic();
     res.json({randomTopic: url});
 }
 
-function randomTopicRedirect (req, res) {
-	var url = randomTopic();
+const randomTopicRedirect = (req, res) => {
+	const url = randomTopic();
     res.redirect(url);
 }
 
-function status (req, res) {
+const status = (req, res) => {
     const version = pkg.version,
         name = pkg.name;
 
     res.json({name, version});
 }
+
+// index.js
+app.get('/', randomTopicHtml);
+app.get('/json', randomTopicJson);
+app.get('/rss', randomTopicRss);
+app.get('/daily', randomTopicDaily);
+app.get('/daily/go', randomTopicDailyRedirect);
+app.get('/daily/rss', randomTopicDailyRss);
+app.get('/go', randomTopicRedirect);
+app.get('/status', status);
 
 module.exports = { randomTopic,  app };
 
